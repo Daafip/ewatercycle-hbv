3a1060e73693baacaf9b362e519e19090721a65f
Daafip <daafips@gmail.com>
2024-02-29 12:49:17 +0100

    fixing test

--- tests/test_forcing.py
+++ tests/test_forcing.py
@@ -4,4 +4,5 @@ from ewatercycle_HBV.forcing import calc_PET
 
 def test_calc_PET():
     """s_rad, t_min, t_max, doy, alpha, elev, lat"""
-    assert calc_PET(np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan) == np.nan # test nans?
+    assert calc_PET(np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0])) == np.array([0]) # test nans?
+    pass
