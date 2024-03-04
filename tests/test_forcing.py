66630a228056ef699d3e9794529d8d9551662ce0
Daafip <daafips@gmail.com>
2024-03-04 12:36:50 +0100

    addin v1.3.10

--- tests/test_forcing.py
+++ tests/test_forcing.py
@@ -4,4 +4,4 @@ from ewatercycle_HBV.forcing import calc_pet
 
 def test_calc_pet():
     """tests s_rad, t_min, t_max, doy, alpha, elev, lat"""
-    assert calc_PET(np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0])) == np.array([0])
+    assert calc_pet(np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0])) == np.array([0])
